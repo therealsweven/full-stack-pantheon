cb6f4be48b6ba83aca0f0b15f12d98c143ba24f7
therealsweven <robssimpson11@gmail.com>
2023-03-22 21:04:17 -0600

    fixed merchant and employee routes, so that session object contains necessary data to add to other tables later.  Started working on table routes

---
+++ controllers/api/tableRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { Tables } = require("../../models");
+
+/* 
+URL route:    /api/tables
+*/
+// Get all table data
+router.get("/", async (req, res) => {
+  try {
+    const tablesData = await Tables.findAll();
+    res.status(200).json(tablesData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Update table data
+router.put("/", async (req, res) => {
+  try {
+    const tableData = await Tables.update(req.body, {
+      where: {
+        id: req.body.id,
+      },
+    });
+  } catch {}
+});
