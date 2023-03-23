628d812c2d3c430c229da22075ddd6e9bde909ad
therealsweven <robssimpson11@gmail.com>
2023-03-22 22:31:27 -0600

    table routes completed and tested

--- controllers/api/tableRoutes.js
+++ controllers/api/tableRoutes.js
@@ -16,14 +16,71 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Add new Table
+router.post("/", async (req, res) => {
+  /* 
+req.body fields:
+
+{
+    table_name: STRING,
+    max_size: INTEGER,
+    available: BOOLEAN,
+}
+
+*/
+  try {
+    req.body.merchant_id = req.session.currentMerchant;
+    const tableData = await Tables.create(req.body);
+    res.status(200).json(tableData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // Update table data
 router.put("/", async (req, res) => {
+  /* 
+req.body fields:
+
+{
+    id: INT,
+    table_name: STRING,
+    available: BOOLEAN,
+}
+
+*/
+
   try {
     const tableData = await Tables.update(req.body, {
       where: {
         id: req.body.id,
       },
     });
+    res.status(200).json(tableData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Delete Table
+router.delete("/", async (req, res) => {
+  /* 
+req.body fields:
+
+{
+    id: INT,
+}
+
+*/
+  try {
+    await Tables.destroy({
+      where: {
+        id: req.body.id,
+      },
+    });
+    res.status(200).json("Table has been deleted.");
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
