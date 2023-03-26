ee52cf8078f5befe8e2b52a51fc8f5910a339b80
therealsweven <robssimpson11@gmail.com>
2023-03-26 15:41:45 -0600

    Merchant settings pretty much done and more allergen seeds added

--- seeds/seed-allergens.js
+++ seeds/seed-allergens.js
@@ -1,15 +1,30 @@
-const { Allergens } = require('../models');
+const { Allergens } = require("../models");
 
 const items = [
-    {
-        type: 'shellfish',
-    },
-    {
-        type: 'milk',
-    },
-    {
-        type: 'nuts',
-    }
+  {
+    type: "shellfish",
+  },
+  {
+    type: "fish",
+  },
+  {
+    type: "milk",
+  },
+  {
+    type: "eggs",
+  },
+  {
+    type: "peanuts",
+  },
+  {
+    type: "tree nuts",
+  },
+  {
+    type: "wheat",
+  },
+  {
+    type: "soy",
+  },
 ];
 
 const seedAllergens = () => Allergens.bulkCreate(items);
