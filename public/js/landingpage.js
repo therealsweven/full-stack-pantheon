b45ca19ebf4c19b0fd9b72893a2e59ed1fd4ccce
Alexandru S <alexandru11723@gmail.com>
2023-03-26 13:55:44 -0600

    merge before save

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -9,6 +9,7 @@ function renderMenuItems(){
         //create image
         var img = document.createElement('img');
         img.setAttribute('src','https://images.unsplash.com/photo-1619096252214-ef06c45683e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80')
-        //
+        //create name paragraph
+        
     }
 }
