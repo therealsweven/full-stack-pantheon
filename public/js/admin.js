b12c50f036f2b3058a999fce5cb14e053adf9781
therealsweven <robssimpson11@gmail.com>
2023-03-27 23:57:46 -0600

    add and delete items done

--- public/js/admin.js
+++ public/js/admin.js
@@ -290,11 +290,12 @@ const deleteEmployeeInDB = async () => {
 // add menu item to database
 const addMenuItem2DB = async () => {
   const itemNameInput = $("#itemNameInput").val().trim();
-  const itemDescriptionIn = $("#employeeEmailInput").val().trim();
-  const itemImageIn = $("#employeeIsManagerIn").val();
-  const itemPriceIn = $("#employeeRoleIn").val();
-  const itemSubtypeIn = $("#employeeRoleIn").val();
-  const itemTypeIn = $("#employeeRoleIn").val();
+  const itemDescriptionIn = $("#itemDescriptionInput").val().trim();
+  const itemImageIn = $("#imageURL").val();
+  const itemPriceIn = $("#itemPriceInput").val();
+  const itemSubtypeIn = $("#itemSubtypeIn").val();
+  const itemTypeIn = $("#itemTypeIn").val();
+  const itemAvail = $("#itemAvailIn").val();
 
   await fetch("/api/menu", {
     method: "POST",
@@ -305,6 +306,8 @@ const addMenuItem2DB = async () => {
       price: itemPriceIn,
       subtype: itemSubtypeIn,
       type: itemTypeIn,
+      available: itemAvail,
+      allergen_ids: [],
     }),
     headers: { "Content-Type": "application/json" },
   });
@@ -342,7 +345,7 @@ const updateMenuItemInDB = async () => {
     body.type = itemSubtypeIn;
   }
   //**************** */
-  if (itemAvail !== "Sub-Type") {
+  if (itemAvail !== "Is Available?") {
     body.available = itemAvail;
   }
   console.log(body);
@@ -358,7 +361,7 @@ const deleteMenuItemInDB = async () => {
   const deleteItemIdInput = Number($("#removeItemIdInput").val().trim());
   await fetch(`/api/menu/`, {
     method: "DELETE",
-    body: JSON.stringify({ id: deleteEmployeeIdInput }),
+    body: JSON.stringify({ id: deleteItemIdInput }),
     headers: { "Content-Type": "application/json" },
   });
 };
